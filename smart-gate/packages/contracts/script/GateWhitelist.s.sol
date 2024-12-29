2ad0a4f4e60f187d18a229b3f5b38dddc6bbece5
NajibAbuSamra <najib.as1990@gmail.com>
2024-12-29 15:30:07 +0200

    adding scripts to whitelist

---
+++ smart-gate/packages/contracts/script/GateWhitelist.s.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import { Script } from "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+import { ResourceId, WorldResourceIdLib } from "@latticexyz/world/src/WorldResourceId.sol";
+import { ResourceIds } from "@latticexyz/store/src/codegen/tables/ResourceIds.sol";
+import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
+import { IBaseWorld } from "@latticexyz/world/src/codegen/interfaces/IBaseWorld.sol";
+
+import { Utils } from "../src/systems/Utils.sol";
+import { SmartGateLib } from "@eveworld/world/src/modules/smart-gate/SmartGateLib.sol";
+import { FRONTIER_WORLD_DEPLOYMENT_NAMESPACE } from "@eveworld/common-constants/src/constants.sol";
+import { Utils as SmartGateUtils } from "@eveworld/world/src/modules/smart-character/Utils.sol";
+import { GateAccessWhitelist } from "../src/codegen/tables/GateAccessWhitelist.sol";
+
+contract GateWhitelist is Script {
+  using SmartGateUtils for bytes14;
+  using SmartGateLib for SmartGateLib.World;
+
+  SmartGateLib.World smartGate;
+
+  function run(address worldAddress) external {
+    // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
+    uint256 privateKey = vm.envUint("PRIVATE_KEY");
+    vm.startBroadcast(privateKey);
+
+    StoreSwitch.setStoreAddress(worldAddress);
+    IBaseWorld world = IBaseWorld(worldAddress);
+
+    smartGate = SmartGateLib.World({ iface: IBaseWorld(worldAddress), namespace: FRONTIER_WORLD_DEPLOYMENT_NAMESPACE });
+
+    uint256 smartGateId = vm.envUint("SOURCE_GATE_ID");
+
+    ResourceId systemId = Utils.smartGateSystemId();
+
+    //This function can only be called by the owner of the smart turret
+    smartGate.configureSmartGate(smartGateId, systemId);
+
+    //Get the allowed corp
+    uint256 size = vm.envUint("NUM_OF_ALLOWED_CORPS");
+    uint256[] memory corpID = new uint256[](size);
+    // for (uint256 i = 0; i < size; i++) {
+    //   corpID[i] = vm.envUint("ALLOWED_CORP_ID_0");
+    // }
+    corpID[0] = vm.envUint("ALLOWED_CORP_ID");
+    //Set the MUD table for the corp whitelist
+    GateAccessWhitelist.set(smartGateId, corpID);
+
+    vm.stopBroadcast();
+  }
+}
